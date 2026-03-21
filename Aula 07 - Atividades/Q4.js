e4b494f2c9a261018eb6a1d844cf7745596b018a
MarcosMenezes24 <quinhotdm@gmail.com>
2026-03-21 00:02:31 -0300

    Quarta questão - OK!

---
+++ Aula 07 - Atividades/Q4.js
@@ -0,0 +1,6 @@
+function converterIdade(anos){
+    anos = parseInt(prompt("Quantos anos você tem?"))
+    let dias = (anos * 365)
+    let meses = (anos * 12)
+    alert(`Você já viveu aproximadamente ${dias} dias e ${meses} meses! `)
+}
