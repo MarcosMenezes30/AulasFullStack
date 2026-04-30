8e1f7bf5ec48a83dc7713bf9e5298f5de549e165
MarcosMenezes30 <quinhotdm@gmail.com>
2026-04-29 23:30:51 -0300

    29/04 changes

--- Unidade 2/Fundamentos NodeJS/index.js
+++ Unidade 2/Fundamentos NodeJS/index.js
@@ -2,7 +2,8 @@ const { Person } = require('./person');
 
 // Essa linha faz com que todos os comandos do arquivo modules/path sejam executados
 // require("./modules/path") 
-require("./modules/fs") 
+// require("./modules/fs") 
+require("./modules/http")
 
 const person = new Person('Marcos');
 
